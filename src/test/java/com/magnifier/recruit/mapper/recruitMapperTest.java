3048ecd0d643e0c27089abfffc0931ff752f6d57
kks1177 <kks1177.dev@gmail.com>
2025-09-02 18:34:29 +0900

    MINOR: 파일명 수정

--- src/test/java/com/magnifier/recruit/mapper/recruitMapperTest.java
+++ src/test/java/com/magnifier/recruit/mapper/recruitMapperTest.java
@@ -1,4 +1,4 @@
-// recruitMapperTest.java
+// RecruitMapperTest.java
 // RecruitMapper 인터페이스의 DB 연동 / MyBatis 연동 기능 테스트 (CRUD 메소드 정상 동작 검증)
 // 전체 채용 공고 조회 기능 테스트
 
@@ -6,21 +6,18 @@ package com.magnifier.recruit.mapper;
 
 import java.sql.SQLException;
 //import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 //import com.magnifier.recruit.dto.RecruitDto;
-
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class recruitMapperTest {
+public class RecruitMapperTest {
     @Autowired
     private RecruitMapper mapper;
 
