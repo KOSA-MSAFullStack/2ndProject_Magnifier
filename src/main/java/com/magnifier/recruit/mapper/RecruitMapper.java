98bd39d88b991e78c6d168aa2d1eca956d75ae9c
kks1177 <kks1177.dev@gmail.com>
2025-09-02 17:37:56 +0900

    FEAT: Recruit Mapper(DAO) 추가

--- src/main/java/com/magnifier/recruit/mapper/RecruitMapper.java
+++ src/main/java/com/magnifier/recruit/mapper/RecruitMapper.java
@@ -1,5 +1,25 @@
+// RecruitMapper.java
+// Mapper(DAO), DB 접근 담당 (SQL 실행)
+
 package com.magnifier.recruit.mapper;
 
-public class RecruitMapper {
+import java.sql.SQLException;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
+import com.magnifier.recruit.dto.RecruitDto;
+
+@Mapper
+public interface RecruitMapper {
+    // 채용 공고 등록
+    void insertRecruit(RecruitDto recruitDto) throws SQLException;
+
+    // 공고 조회
+    List<RecruitDto> getRecruitList() throws SQLException;
+
+    // 공고 수정
+    int updateRecruit(RecruitDto recruitDto) throws SQLException;
+
+    // 공고 삭제
+    int deleteRecruit(RecruitDto recruitDto) throws SQLException;
 }
