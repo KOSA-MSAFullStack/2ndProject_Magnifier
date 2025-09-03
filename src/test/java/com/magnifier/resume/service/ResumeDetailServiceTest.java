5465aed3aa1e018d885714c1006a7ea4ff81824c
lsw1096 <lsw1096@gmail.com>
2025-09-03 17:45:19 +0900

    FEAT: 이력서(경력사항, 자격사항) 상세 조회하는 기능 추가

---
+++ src/test/java/com/magnifier/resume/service/ResumeDetailServiceTest.java
@@ -0,0 +1,36 @@
+package com.magnifier.resume.service;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.magnifier.resume.dto.ResumeDto;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ResumeDetailServiceTest {
+
+	@Autowired
+    private ResumeService service;
+	
+	@Test
+    public void testFindResumesByMemberId() {
+        int memberId = 2;
+        
+        List<ResumeDto> resumes = service.findResumesByMemberId(memberId);
+        
+        log.info("--- 회원 ID " + memberId + "에 대한 이력서 조회 결과 ---");
+        for (ResumeDto resume : resumes) {
+            log.info(resume);
+            log.info("Career: " + resume.getCareerList());
+            log.info("License: " + resume.getLicenseList());
+        }
+    }
+}
