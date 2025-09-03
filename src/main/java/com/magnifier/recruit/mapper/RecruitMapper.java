113e2379bf3ad1c18c28f37892d03cf7577524a0
kks1177 <kks1177.dev@gmail.com>
2025-09-03 12:28:08 +0900

    FEAT: Mapper 파일 수정

--- src/main/java/com/magnifier/recruit/mapper/RecruitMapper.java
+++ src/main/java/com/magnifier/recruit/mapper/RecruitMapper.java
@@ -11,15 +11,18 @@ import com.magnifier.recruit.dto.RecruitDto;
 
 @Mapper
 public interface RecruitMapper {
-    // 채용 공고 등록
-    int insertRecruit(RecruitDto recruitDto) throws SQLException;
-
     // 채용공고 전체 목록 조회
     List<RecruitDto> getRecruitList() throws SQLException;
 
-    // 공고 수정
+    // 채용 공고 등록 (C)
+    int insertRecruit(RecruitDto recruitDto) throws SQLException;
+
+    // 상세보기 (R)
+    RecruitDto detailRecruit(int recruitId) throws SQLException;
+
+    // 공고 수정 (U)
     int updateRecruit(RecruitDto recruitDto) throws SQLException;
 
-    // 공고 삭제
+    // 공고 삭제 (D)
     int deleteRecruit(int recruitId) throws SQLException;
 }
