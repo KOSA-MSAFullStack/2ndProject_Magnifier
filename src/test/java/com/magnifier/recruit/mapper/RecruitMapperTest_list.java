62b5bacb2d146f0664f4d9e3027fce37237b0360
kks1177 <kks1177.dev@gmail.com>
2025-09-04 02:00:06 +0900

    TEST: getRecruitList 테스트에 검증 로직 추가

--- src/test/java/com/magnifier/recruit/mapper/RecruitMapperTest_list.java
+++ src/test/java/com/magnifier/recruit/mapper/RecruitMapperTest_list.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import lombok.extern.log4j.Log4j;
+import static org.junit.Assert.assertNotNull;
+import java.util.List;
+
+import com.magnifier.recruit.dto.RecruitDto;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -21,7 +25,16 @@ public class RecruitMapperTest_list {
 
     @Test
     public void testGetList() throws SQLException {
+        // DB에서 전체 리스트 조회
+        List<RecruitDto> list = mapper.getRecruitList();
+
+        // 리스트가 null이 아닌지 검증
+        assertNotNull(list);
+
         // forEach와 람다 표현식 사용, 목록 순회하며 각 공고 정보 로그로 출력
-        mapper.getRecruitList().forEach(board -> log.info(board));
+        list.forEach(board -> log.info(board));
+
+        log.info("----------");
+        log.info("전체 공고 수: " + list.size());
     }
 }
