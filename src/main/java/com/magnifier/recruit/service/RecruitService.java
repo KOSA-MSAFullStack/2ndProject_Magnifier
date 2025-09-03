97aa267b0836f377ea5c157c3e5ffac97cd0ee91
kks1177 <kks1177.dev@gmail.com>
2025-09-03 12:56:36 +0900

    FEAT: Recruit Service 추가

--- src/main/java/com/magnifier/recruit/service/RecruitService.java
+++ src/main/java/com/magnifier/recruit/service/RecruitService.java
@@ -1,8 +1,25 @@
 // RecruitService.java
-// Service, 비즈니스 로직 처리 담당 (여러 DAO/Mapper 조합해서 하나의 기능 구현) + 트랜잭션 처리
+// Service 인터페이스, 비즈니스 로직 처리 담당 (서비스 기능 선언, 실제 구현 x)
 
 package com.magnifier.recruit.service;
 
-public interface RecruitService {
+import java.sql.SQLException;
+import java.util.List;
+import com.magnifier.recruit.dto.RecruitDto;
 
+public interface RecruitService {
+    // 채용공고 전체 목록 조회
+    List<RecruitDto> getRecruitList() throws SQLException;
+
+    // 채용 공고 등록 (C)
+    int insertRecruit(RecruitDto recruitDto) throws SQLException;
+
+    // 상세보기 (R)
+    RecruitDto detailRecruit(int recruitId) throws SQLException;
+
+    // 공고 수정 (U)
+    int updateRecruit(RecruitDto recruitDto) throws SQLException;
+
+    // 공고 삭제 (D)
+    int deleteRecruit(int recruitId) throws SQLException;
 }
