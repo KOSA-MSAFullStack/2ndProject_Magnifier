40e8ba6b2bc514205e3f440db1972b0b84827394
kks1177 <kks1177.dev@gmail.com>
2025-09-15 15:21:08 +0900

    FEAT: 채용 공고 지원하기 기능 구현

---
+++ src/main/java/com/magnifier/applylist/mapper/ApplylistInsertMapper.java
@@ -0,0 +1,20 @@
+// ApplylistInsertMapper.java
+// 지원 목록 삽입 매퍼 인터페이스
+
+package com.magnifier.applylist.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.magnifier.applylist.entity.Applylist;
+
+// * author: 김기성
+@Mapper
+public interface ApplylistInsertMapper {
+
+	// 지원 정보 추가
+	void insertApplylist(Applylist applylist);
+
+	// 회원 ID와 채용 공고 ID로 지원 내역 조회
+	Applylist selectApplylistByMemberAndRecruit(@Param("memberId") Integer memberId, @Param("recruitId") Integer recruitId);
+}
--- src/main/java/com/magnifier/applylist/service/ApplylistService.java
+++ src/main/java/com/magnifier/applylist/service/ApplylistService.java
@@ -1,15 +1,24 @@
+// ApplylistService.java
+// 지원 목록 서비스 인터페이스
+
 package com.magnifier.applylist.service;
 
 import java.util.List;
 
 import com.magnifier.applylist.dto.EnterpriseApplylistDto;
 import com.magnifier.applylist.dto.MemberApplylistDto;
+import com.magnifier.applylist.entity.Applylist;
 
 public interface ApplylistService {
+	// * author: 김기성
+	// 지원 정보 추가
+	public void insertApplylist(Applylist applylist);
 	
-	// 회원 지원내역 조회
+	// * author: 이상우
+	// 개인 회원 - 개인이 지원한 내역 조회
 	public List<MemberApplylistDto> selectApplylistsByMemberId(int memberId);
 	
-	// 기업 지원내역 조회
+	// * author: 이상우
+	// 기업 회원 - 기업에 지원한 내역 조회
 	public List<EnterpriseApplylistDto> selectApplylistsByEnterpriseId(int enterpriseId);
 }
